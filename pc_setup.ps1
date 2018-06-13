500946f8751187282581ef7ca842bdab04ec9ff5
Carl Meyertons <carlmeyertons@gmail.com>
2018-06-13 12:46:59 -0500

    changing over to powershell

--- pc_setup.ps1
+++ pc_setup.ps1
@@ -1,4 +1,4 @@
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 start https://aka.ms/azureml-wb-msi
 start https://www.visualstudio.com/downloads/
@@ -6,15 +6,15 @@ start https://www.visualstudio.com/downloads/
 choco feature enable -n allowGlobalConfirmation
 choco install notepadplusplus
 choco install wox
-choco install crystaldiskmark
-choco install crystaldiskinfo
+# choco install crystaldiskmark
+# choco install crystaldiskinfo
 choco install autohotkey
 choco install everything
 choco install 7zip
 choco install vscode
-choco install git /NoShellIntegration
+choco install git
 choco install pia
-choco install qbittorrent
+# choco install qbittorrent
 choco install vlc
 choco install google-backup-and-sync
 choco install github
@@ -22,8 +22,12 @@ refreshenv
 
 git config --global user.name = "Carl Meyertons"
 git config --global user.email = "carlmeyertons@gmail.com"
+# workaround for github desktop defaulting to HTTPS...
+git config --global url."git@github.com:".insteadOf https://github.com/
 
+# generate SSH key (may need to be done in bash script)
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\usr\bin", [EnvironmentVariableTarget]::User)
+refreshenv
 ssh-keygen -t rsa -b 4096 -C "carlmeyertons@gmail.com"
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
-
